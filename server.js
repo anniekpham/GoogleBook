ef33a7a87f202df1f18eb9dadc55701881dddfbd
Annie Pham <anniekpham@yahoo.com>
2019-06-25 19:57:03 -0700

    added mlab

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/books_db";
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -8,6 +10,7 @@ app.use(express.json())
 require('./routes')(app)
 
 
-require('mongoose').connect('mongodb://localhost/books_db', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+
 .then(_ => app.listen(PORT))
 .catch(e => console.log(e))
