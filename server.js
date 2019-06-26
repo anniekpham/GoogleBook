95461f539b45b55969150f57f91e7dd5db150a61
Annie Pham <anniekpham@yahoo.com>
2019-06-25 20:24:08 -0700

    something

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
+const { join } = require('path')
 const PORT = process.env.PORT || 3001
 const mongoose = require('mongoose')
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/books_db";
 
+app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 require('./routes')(app)
 
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname + '/client/src/index.html'))
+// })
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
 
